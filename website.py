a86457949610a890d24bde16fa42a6b752174427
Afreecuh <william921127@gmail.com>
2025-02-26 02:36:36 +0800

    Fix Google Analytics injection and UI issues

--- website.py
+++ website.py
@@ -454,7 +454,7 @@ def plot_shape_composition_bar(ratios):
     return fig
 
 
-# In[7]:
+# In[8]:
 
 
 import streamlit as st
@@ -466,20 +466,18 @@ import numpy as np
 import streamlit.components.v1 as components
 
 def inject_ga():
-    """Inject Google Analytics tracking code into Streamlit."""
-    GA_TRACKING_ID = "G-4QWR3D46SD"
+    """Inject Google Analytics tracking code into the Streamlit app."""
+    GA_TRACKING_ID = "G-4QWR3D46SD"  # 你的 Google Analytics 代碼
     ga_code = f"""
     <script async src="https://www.googletagmanager.com/gtag/js?id={GA_TRACKING_ID}"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
         function gtag(){{dataLayer.push(arguments);}}
         gtag('js', new Date());
-        gtag('config', '{GA_TRACKING_ID}', {{'send_page_view': true}});
+        gtag('config', '{GA_TRACKING_ID}', {{ 'send_page_view': true }});
     </script>
     """
-    st.markdown(f"<script>{ga_code}</script>", unsafe_allow_html=True)  # 用 markdown 方式插入 JS
-
-inject_ga()
+    components.html(ga_code, height=0)  # **確保這行執行，而不是用 st.code() 顯示**
 
 
 def upload_image():
